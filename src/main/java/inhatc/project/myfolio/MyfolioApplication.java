66d0cbea8817475950e401c0dd1977eba900ec4c
bestinwoo <bestinwoo@gmail.com>
2022-12-02 21:37:45 +0900

    first commit

---
+++ src/main/java/inhatc/project/myfolio/MyfolioApplication.java
@@ -0,0 +1,13 @@
+package inhatc.project.myfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inhatc/project/myfolio/MyfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package inhatc.project.myfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
