368e1e68e314f7a52e5e7cf6818a73a7726abfb7
bestinwoo <bestinwoo@gmail.com>
2022-12-11 00:37:46 +0900

    feat(ProjectMapper): Project List -> ProjectDto List Mapping 추가

--- src/main/java/inhatc/project/myfolio/project/mapper/ProjectMapper.java
+++ src/main/java/inhatc/project/myfolio/project/mapper/ProjectMapper.java
@@ -1,11 +1,17 @@
 package inhatc.project.myfolio.project.mapper;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import inhatc.project.myfolio.project.ProjectDto;
 import inhatc.project.myfolio.project.domain.Project;
+import inhatc.project.myfolio.tag.TagDto;
+import inhatc.project.myfolio.tag.domain.ProjectTag;
 
 @Mapper
 public interface ProjectMapper {
@@ -17,7 +23,17 @@ public interface ProjectMapper {
 	Project projectDtoToProject(
 			ProjectDto.Request.Create projectDto);
 
-	ProjectDto.Response projectToProjectDto(
+
+	ProjectDto.Response.Detail projectToProjectDetail(
 			Project project);
 
+	List<ProjectDto.Response.Summary> toProjectSummaries(List<Project> projects);
+
+	default Set<TagDto> projectTagToTagDto(Set<ProjectTag> tags) {
+		Set<TagDto> tagDtoSet = new HashSet<>();
+		for (ProjectTag tag : tags) {
+			tagDtoSet.add(new TagDto(tag.getTag().getId(), tag.getTag().getName()));
+		}
+		return tagDtoSet;
+	}
 }
