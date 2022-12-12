1c131fd4d93452481f98ba981c18bcfb515f793c
bestinwoo <bestinwoo@gmail.com>
2022-12-12 14:00:10 +0900

    test(ProjectRepository): 전체 테스트 수행시 insert error 발생 해결

--- src/test/java/inhatc/project/myfolio/project/repository/ProjectRepositoryTest.java
+++ src/test/java/inhatc/project/myfolio/project/repository/ProjectRepositoryTest.java
@@ -2,10 +2,17 @@ package inhatc.project.myfolio.project.repository;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.sql.DataSource;
+
+import org.aspectj.lang.annotation.Before;
+import org.h2.tools.Server;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +29,7 @@ import inhatc.project.myfolio.member.repository.MemberRepository;
 import inhatc.project.myfolio.project.domain.Project;
 import inhatc.project.myfolio.tag.domain.ProjectTag;
 import inhatc.project.myfolio.tag.domain.Tag;
+import inhatc.project.myfolio.tag.repository.ProjectTagRepository;
 import inhatc.project.myfolio.tag.repository.TagRepository;
 
 @DataJpaTest
@@ -33,10 +41,12 @@ class ProjectRepositoryTest {
 	@Autowired
 	private TagRepository tagRepository;
 	@Autowired
+	private ProjectTagRepository projectTagRepository;
+	@Autowired
 	private MemberRepository memberRepository;
 
-	public void createProjects() {
-		ArrayList<Project> projects = new ArrayList<>();
+	@BeforeEach
+	void createProjects() {
 		for(long i = 1; i <= 10; i++) {
 			String prefix = i % 3 == 0 ? "Before" : "After";
 			Tag tag = Tag.builder().name(prefix + " Tag " + i).build();
@@ -45,25 +55,24 @@ class ProjectRepositoryTest {
 			memberRepository.save(member);
 
 			Project project = Project.builder()
-					.id(i)
 					.githubUrl("Test Github " + i)
 					.thumbnailUrl("Test Thumbnail " + i)
 					.content("Test Content " + i)
 					.title(prefix + " Title " + i)
 					.webUrl("Test Web " + i)
 					.member(member)
-					.tags(Set.of(
-							ProjectTag.builder().id(i).tag(tag).build()))
 					.build();
-			projects.add(project);
+			projectRepository.save(project);
+
+			ProjectTag pt = ProjectTag.builder().tag(tag).project(project).build();
+			projectTagRepository.save(pt);
+			project.setTags(Set.of(pt));
 		}
-		projectRepository.saveAll(projects);
 	}
 
 	@Test
 	@DisplayName("프로젝트 제목 검색 테스트")
 	public void searchProjectTitle() {
-		createProjects();
 		PageRequest pageRequest = PageRequest.of(0, 5, Sort.by("createdDate"));
 		Page<Project> projects = projectRepository.findByTitleContaining("After", pageRequest);
 
@@ -74,7 +83,6 @@ class ProjectRepositoryTest {
 	@Test
 	@DisplayName("프로젝트 태그 검색 테스트")
 	public void searchProjectTag() {
-		createProjects();
 		PageRequest pageRequest = PageRequest.of(0, 5, Sort.by("createdDate"));
 		Page<Project> projects = projectRepository.findPageByTagName("After", pageRequest);
 
